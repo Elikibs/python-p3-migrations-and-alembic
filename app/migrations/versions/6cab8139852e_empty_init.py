c6924e6c5f51e600339a72bd797e7c57ece69fc1
Elisha <elishakibet67@gmail.com>
2024-01-04 17:38:00 +0300

    Managing migrations

---
+++ app/migrations/versions/6cab8139852e_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: 6cab8139852e
+Revises: 
+Create Date: 2024-01-03 23:52:01.270830
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '6cab8139852e'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
